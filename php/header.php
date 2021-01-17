e416733903a07a8019551e2d8275d8160a635d51
MVour <m.vourtzoumis@gmail.com>
2021-01-17 23:24:05 +0200

    register updates

--- php/header.php
+++ php/header.php
@@ -11,11 +11,12 @@
 		function openForm() {
 			// document.getElementById("body_1").style.opacity = "0.2";
 			document.getElementById("reg").style.display = "flex";
-			document.getElementById("myRegister").style.display = "flex";
-			document.documentElement.style.overflow = 'hidden';
+			document.getElementById("myRegister").style.display = "block";
+			// document.documentElement.style.overflow = 'hidden';
 			document.body.scroll = "no";
 			document.getElementById("register_form").reset();
 			resetSelect();
+			reset_buisness();
 			// document.getElementById("myRegister").style.opacity = "1";
 		}
 
@@ -93,7 +94,7 @@
 							<div class="login_submit_container">
 								<input type="submit" style="" value="Είσοδος">
 							
-							<div role="button" class="register_button" onclick="openForm()">Εγγραφή</div>
+								<div role="button" class="register_button" onclick="openForm()">Εγγραφή</div>
 							</div>
 						</form>
 					</div>
@@ -101,56 +102,43 @@
 			</div>
 		</div>
 	</head>
-    <div class="register" id="reg">
-        </div>
-        <div class="register_popup" id="myRegister">
-            <form action="./header.html" id="register_form" class="register_container">
-                <h1><b>Εγγραφή</b></h1>
-				<h6>Εύκολα και γρήγορα!</h6>
-					<div class="x_button">
-						<img role="button" src="../include/images/x_button.png" onclick="closeForm()">
-					</div>
+	<div class="register" id="reg"></div>
+    <div class="register_popup" id="myRegister">
+		<div class="x_button">
+			<img role="button" src="../include/images/x_button.png" onclick="closeForm()">
+		</div>
+		<div class="register_welcome">
+			<div class="register_welcome1"><b>Εγγραφή<b></div>
+			<div class="register_welcome2">Εύκολα και γρήγορα!</div>
+		</div>
+		<div class="register_container">
+			<!-- GEIA -->
+			<form action="./homepage.php" method="post" id="register_form" >
+				<input type="text" placeholder="Επώνυμο" name="l_name" required style="width:44%;">
 				
-					<div class="together">
-						<input type="text" placeholder="Επώνυμο" name="l_name" required style="width:44%;">
-						<input type="text" placeholder="Όνομα" name="f_name" required style="width:44%;">
-					</div>
-					<div class="alone">
-						<input type="text" placeholder="E-mail" name="email" required>
-					</div>
-					<div class="alone">
-						<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Αριθμός Κινητού"required>
-					</div>
-					<div class="together">
-						<input type="text" placeholder="Κωδικός" name="psswrd" required style="width:44%;">
-						<input type="text" placeholder="Επιβεβαίωση Κωδικού" name="" required style="width:44%;">
-					</div>
-					<div class="together">
-						<select id="user_type" name="user_type" style="width:90%;color:gray" onclick="changeSelect()">
-							<option value="" hidden style="display:none">Κατηγορία Χρήστη</option>
-							<option value="1">Εργοδότης</option>
-							<option value="2">Εργαζόμενος</option>
-							<option value="3">'Ανεργος</option>
-						</select>
-						<div class="together" style="border:2px solid red;">
-							<select id="user_type" name="user_type" style="width:44%;color:gray" onclick="changeSelect();check_UserType();">
-								<option value="" hidden style="display:none">Κατηγορία Χρήστη</option>
-								<option value="1">Εργοδότης</option>
-								<option value="2">Εργαζόμενος</option>
-								<option value="3">'Ανεργος</option>
-							</select>
-							<input type="text" placeholder="Δήλωση Επιχείρησης" id="add_b" name="add_b" class="add_business" style="width:44%;">
-							<select id="select_b" name="select_b" class="select_business;" style="width:44%;" onChange="changeSelect();">
-								<option value="" hidden style="display:none;">Επιλογή Επιχείρησης</option>
-								<?php
-									include("../php/main_functs.php");
-									get_business();
-								?>
-								<option value="1">#1</option>
-							</select>
-						</div>
-					</div>
-					<input type="submit" value="Εγγραφή" style="height:60%; text-align:center;">
-            </form>
-        </div>
+				<input type="text" placeholder="Όνομα" name="f_name" required style="width:44%;">
+				<input type="text" placeholder="E-mail" name="email" required><br>
+				<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Αριθμός Κινητού"required><br>
+				<input type="text" placeholder="Κωδικός" name="psswrd" required style="width:44%;">
+				<input type="text" placeholder="Επιβεβαίωση Κωδικού" name="" required style="width:44%;">
+				<select id="user_type" name="user_type" style="width:44%;color:gray" onChange="changeSelect();check_UserType();">
+					<option value="" hidden style="display:none">Κατηγορία Χρήστη</option>
+					<option value="1">Εργοδότης</option>
+					<option value="2">Εργαζόμενος</option>
+					<option value="3">'Ανεργος</option>
+				</select>
+				
+					<input type="text" placeholder="Δήλωση Επιχείρησης" id="add_b" name="add_b" style="width:44%;display:none">
+					<select id="select_b" name="select_b" style="width:44%;display:none;" onChange="changeSelect();">
+						<option value="" hidden style="display:none;">Επιλογή Επιχείρησης</option>
+						<?php
+							include("../php/main_functs.php");
+							$mpla = get_business();
+						?>
+					</select>
+				<input type="submit" value="Εγγραφή">
+
+				</form>
+		</div>
+	</div>
 </html>
