8387121f085931c82b2f9c7261af15dbfd32fbc6
MVour <m.vourtzoumis@gmail.com>
2021-01-18 20:44:16 +0200

    footer

---
+++ php/footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+<homepage>
+<html>
+	<meta charset="UTF-8">
+    <link rel= "stylesheet" href="../css/footer_style.css?v=1.1">
+    <footer>
+        <div class="footer_container">
+            <div class="footer_left">
+                <div class="title_container">
+                    <div class="p1">
+                        <b>ΥΠΟΥΡΓΕΙΟ</b>
+                    </div>
+                </div>
+                <ul>
+                    <li><a href='#'>Ιστορική Εξέλιξη</a></li>
+                    <li><a href='#'>Έργο</a></li>
+                    <li><a href='#'>Οργανόγραμμα</a></li>
+                </ul>
+            </div>
+            <div class="footer_right ">
+                <div class="p1" style="text-align:right;float:right;">
+                    <b>ΕΠΙΚΟΙΝΩΝΙΑ</b>
+                </div>
+                <ul style="float:right;">
+                    <li><a href='#'>Ιστορική Εξέλιξη</a></li>
+                    <li><a href='#'>Έργο</a></li>
+                    <li><a href='#'>Οργανόγραμμ α</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </footer>
+
+
+</html>
+
+</homepage>
